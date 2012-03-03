073fb62096899a52b885f01b8a2222363f63db3a
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-03 16:41:19 +0100

    Don't need .5

--- examples/ex4.js
+++ examples/ex4.js
@@ -1,7 +1,7 @@
 trolley.init();
 trolley.body(0, 0, true).box(15, 1);
 trolley.body(5, 1).box(2, 2);
-trolley.body(4.5, 15, {
+trolley.body(4, 15, {
     fixedRotation: true
 }).box(2, 2, {
     density: 100
