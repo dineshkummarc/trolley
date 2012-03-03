0d56a39c411918d12dbc7f438650634c0b4eccf4
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-03 12:15:37 +0100

    New examples

--- examples/ex2.js
+++ examples/ex2.js
@@ -1,3 +1,8 @@
 trolley.init();
-trolley.body(0, 0, true).box(50, 1).box();
-trolley.body(10, 10).box(2, 2);
+trolley.body(0, 0, true).box(50, 1);
+trolley.body(10, 10).box(1, 1);
+trolley.body(15, 10).box(1, 2, {
+    density: 2,
+    rotate: false
+}).circle(0, 2, 1);
+
