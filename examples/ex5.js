761ecb73fa6d091ae80aee9884ce57daa2a03327
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-10 15:12:14 +0100

    New layout for joints

--- examples/ex5.js
+++ examples/ex5.js
@@ -2,4 +2,4 @@ trolley.init();
 trolley.body(0, 8, true).box(15, 1);
 var b1 = trolley.body(4, 5).circle(2).body;
 var b2 = trolley.body(6, 12).circle(2).body;
-trolley.joint(b1, 1, 1, b2, 1, 1);
+trolley.joint().a(b1, 1, 1).b(b2, 1, 1);
--- examples/ex6.js
+++ examples/ex6.js
@@ -3,5 +3,5 @@ trolley.body(6, 10, true).box(18, 1);
 var b1 = trolley.body(15, 20).circle(2).body;
 var b2 = trolley.body(13, 10).circle(2).body;
 var b3 = trolley.body(17, 10).circle(2).body;
-trolley.joint(b1, 1, 1, b2, 1, 1);
-trolley.joint(b1, 1, 1, b3, 1, 1);
+trolley.joint().a(b1, 1, 1).b(b2, 1, 1);
+trolley.joint().a(b1, 1, 1).b(b3, 1, 1);
